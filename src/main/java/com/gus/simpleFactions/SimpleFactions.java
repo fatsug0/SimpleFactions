8564eea039c84af27b885380a4f983900ba9bc17
Fatsug <fatsug.ytb@gmail.com>
2025-12-15 22:28:45 +0100

    Initial Commit

---
+++ src/main/java/com/gus/simpleFactions/SimpleFactions.java
@@ -0,0 +1,17 @@
+package com.gus.simpleFactions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleFactions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
